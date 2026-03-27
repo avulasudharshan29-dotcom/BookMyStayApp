93731ff905ce1f7ff9358cd47016c62c3aabdc4a
https://github.com/avulasudharshan29-dotcom <avulasudharshan29@gmail.com>
2026-03-27 09:08:56 +0530

    done

---
+++ APP/src/ApplicationEntry.java
@@ -0,0 +1,28 @@
+/**
+ * HotelBookingApp.java
+ *
+ * This class represents the entry point of the Hotel Booking application.
+ * It demonstrates how a Java program begins execution and produces console output.
+ *
+ * @author YourName
+ * @version 1.0
+ */
+public class ApplicationEntry {
+
+    /**
+     * The main method is the entry point of every standalone Java application.
+     * The JVM looks specifically for this method signature to begin execution.
+     *
+     * @param args Command-line arguments passed to the application
+     */
+    public static void main(String[] args) {
+        // Print a welcome message to the console
+        System.out.println("Welcome to the Hotel Booking System!");
+
+        // Display application name and version
+        System.out.println("Application: Hotel Booking System");
+        System.out.println("Version: 1.0");
+
+        // Program terminates after printing messages
+    }
+}
